af5263f94e85d38d7dd1e003a37b59081b147816
M <69633813+martin-iturralde@users.noreply.github.com>
2023-11-14 16:42:22 +0100

    Delete venv_name directory

--- venv_name/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/martin/Desktop/ECM/S7/Informatique/Info 2/djangology/venv_name/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
